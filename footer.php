ea83e0f733784c9dc93ac74a735bbf07dd048c11
David Durant <dave@bowsy.co.uk>
2025-10-06 17:32:30 +0100

    Changed volunteer link to a mailto: rather than a direct link to the WhatsApp

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <footer role="contentinfo">
       <div class="footer-top">
         <a href="https://transforgov-talks.myspreadshop.co.uk/all" target="_blank" rel="noopener">Merch</a> |
-        <a href="https://chat.whatsapp.com/JSLuWXEFEZZGlpIIHa0y0a" target="_blank" rel="noopener">Volunteer</a> |
+        <a href="mailto:transformsgovtalks@gmail.com?subject=Volunteering to help" target="_blank">Volunteer</a> |
         <a href="/resources/poster.pdf" target="_blank" rel="noopener">Download a Poster</a> |
         <a href="https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=Nm9tNGtnNzFhNTI4ZWk2NXZrZmJzcHAza3ZfMjAyNTA0MzBUMTcwMDAwWiB0cmFuc2Zvcm1nb3Z0YWxrc0Bt&amp;tmsrc=transformgovtalks%40gmail.com&amp;scp=ALL" target="_blank" rel="noopener">Calendar Hold</a>
       </div>
